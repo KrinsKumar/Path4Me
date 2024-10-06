1c15a0ac0db09aa9b099a91c906504252608f209
Shrey Bhatt <shreybhatt130@yahoo.com>
2024-10-06 09:46:31 -0400

    pitch

--- raspberryPiModule/utils/sound.py
+++ raspberryPiModule/utils/sound.py
@@ -5,9 +5,9 @@ import time
 
 # Parameters
 duration = 1  # seconds
-sampling_rate = 11050  # samples per second (standard for audio)
+sampling_rate = 44100  # samples per second (standard for audio)
 frequency = 220.0  # frequency of the sound (A4)
-chunk_size = 2048  # Increased chunk size to reduce underrun errors
+chunk_size = 1024  # Increased chunk size to reduce underrun errors
 current_byte = 0
 
 # Generate the waveform for the entire duration
@@ -78,9 +78,6 @@ def create_sound():
         stream.stop_stream()
         if beep:
             time.sleep(0.5)
-            beep = False
-        else:
-            time.sleep(0.1)
         stream.start_stream()
 
 if __name__ == "__main__":
